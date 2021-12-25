79a2864f261fc1fc2813287fb4ce4888439a6b11
Doğan Can Selek <canselek2434@gmail.com>
2021-12-25 14:21:56 +0300

    add styles

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import "./styles.css"
 import Form from './Form'
 import List from './List'
 
 function Contacts() {
-    const [contacts, setContacts] = useState([])
+    const [contacts, setContacts] = useState([
+        { fullname: "Can Selek", phone_number: "123123" },
+        { fullname: "Umut", phone_number: "9876" },
+        { fullname: "Ata", phone_number: "456334" }
+    ])
 
     useEffect(() => {
         console.log(contacts)
     }, [contacts])
     return (
-        <div>
+        <div id='container'>
+            <h1>dcselek's Contacts</h1>
             <List contacts={contacts} />
+            <h2>Contacts Length = {contacts.length}</h2>
             <Form setContacts={setContacts} contacts={contacts} />
         </div>
     )
