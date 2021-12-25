9525fe73d2a8e0667227ac8096b3c01806cbce1b
Doğan Can Selek <canselek2434@gmail.com>
2021-12-25 11:22:10 +0300

    set registiration for contacts

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Form() {
-    const [form, setForm] = useState({ fullname: "", phone_number: "" })
+const initialForm = { fullname: "", phone_number: "" }
+function Form({ contacts, setContacts }) {
+    const [form, setForm] = useState(initialForm)
+
+    useEffect(() => {
+        setForm(initialForm)
+    }, [contacts])
 
     const onChangeInput = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
-    const onSubmit = () => {}
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (form.fullname === "" || form.phone_number === "") {
+            return false
+        }
+        setContacts([...contacts, form])
+    }
     return (
         <form onSubmit={onSubmit}>
             <div>
-                <input name="fullname" placeholder='Full Name' onChange={onChangeInput} />
+                <input
+                    name="fullname"
+                    placeholder='Full Name'
+                    onChange={onChangeInput}
+                    value={form.fullname}
+                />
             </div>
             <div>
-                <input name="phone_number" placeholder='Phone Number' onChange={onChangeInput} />
+                <input
+                    name="phone_number"
+                    placeholder='Phone Number'
+                    onChange={onChangeInput}
+                    value={form.phone_number}
+                />
             </div>
             <div>
                 <button>Add</button>
