18e20749b6e5a313933435547bd74eb5e68ff5f1
Doğan Can Selek <canselek2434@gmail.com>
2021-12-25 11:02:17 +0300

    add Contacts, Form, List

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function Form() {
+    const [form, setForm] = useState({ fullname: "", phone_number: "" })
+
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = () => {}
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <input name="fullname" placeholder='Full Name' onChange={onChangeInput} />
+            </div>
+            <div>
+                <input name="phone_number" placeholder='Phone Number' onChange={onChangeInput} />
+            </div>
+            <div>
+                <button>Add</button>
+            </div>
+        </form>
+    )
+}
+
+export default Form
